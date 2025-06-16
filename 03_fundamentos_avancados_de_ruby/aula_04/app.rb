01b99c7c3d9491a576db0b194e9f907918fcdf3e
ericdaher <ericdaher2@gmail.com>
2025-06-16 07:12:44 -0300

    class 03 exercise 04 (wip)

---
+++ 03_fundamentos_avancados_de_ruby/aula_04/app.rb
@@ -0,0 +1,71 @@
+# Exercício: ThreadPool dinâmica com fila de prioridade 
+
+# Contexto: 
+# Você está desenvolvendo um sistema de processamento de tarefas em background que deve lidar com múltiplas 
+# prioridades. Para isso, deseja implementar sua própria ThreadPool dinâmica, capaz de: 
+# - Executar tarefas concorrentes com múltiplas Threads. 
+# - Usar filas de prioridade (:high, :medium, :low) para gerenciar a ordem de execução 
+# - Permitir o aumento ou redução do número de Threads ativamente consumindo tarefas 
+
+# Objetivo: 
+# Implemente as seguintes classes: 
+# 1. PriorityQueue 
+# Uma classe thread-safe que: 
+# - Mantém várias filas (Queue) associadas a diferentes prioridades 
+# - Permite adicionar tarefas com prioridade específica 
+# - Retorna a próxima tarefa disponível com a mais alta prioridade disponível 
+# 2. DynamicThreadPool 
+# Uma classe com: 
+# - Um número inicial de Threads (ex: 2). 
+# - Capacidade de crescer até um número máximo de Threads (ex: até 10) dinamicamente 
+# - Acesso a métodos como schedule(priority = :medium, &block) para adicionar tarefas. 
+# - Remoção de threads mortas dinamicamente 
+# Exemplo de uso: 
+# pool = DynamicThreadPool.new(max_threads: 3) 
+# 10.times do |i| 
+#   pool.schedule(:default) { sleep 1; puts "Tarefa padrão #{i} concluída" } 
+# end 
+# 5.times do |i| 
+#   pool.schedule(:high) { sleep 0.5; puts "Tarefa prioritária #{i} concluída" } 
+# end 
+# pool.shutdown  # => termina a execução de todas as Threads
+
+class PriorityQueue
+  PRIORITIES = %i(high medium low)
+
+  def initialize
+    @internal_queues = {}
+    PRIORITIES.each { |priority| @internal_queues[priority] = Queue.new }
+    @mutex = Mutex.new
+  end
+
+  def add(priority, &task)
+    @internal_queues[priority.to_sym].push(task)
+  end
+
+  def next_task
+    @mutex.synchronize do
+      task = if !@internal_queues[:high].empty?
+        @internal_queues[:high].pop
+      elsif !@internal_queues[:medium].empty?
+        @internal_queues[:medium].pop
+      elsif !@internal_queues[:low].empty?
+        @internal_queues[:low].pop
+      end
+
+      return 'No task to run' if task.nil?
+      task.yield
+    end
+  end
+end
+
+priority_queue = PriorityQueue.new
+
+priority_queue.add(:medium) { puts 'Normal task' }
+priority_queue.add(:high) { puts 'This should run first' }
+priority_queue.add(:low)  { puts 'This should run last' }
+
+priority_queue.next_task # This should run first
+priority_queue.next_task # Normal task
+priority_queue.next_task # This should run last
+priority_queue.next_task # No task to run
